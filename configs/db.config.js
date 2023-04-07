aff3b30e4fac05f589768aaa2786b9c1241a2fae
Prathamesh Lakhpati <prathmeshlakhpaty@gmail.com>
2023-04-07 19:46:24 +0530

    Initial setup for the project and creation of restaurant schema

---
+++ configs/db.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  DB_NAME: "find-my-restaurant-db",
+  DB_URL:
+    "mongodb+srv://prathmeshlakhpaty:9764prath123@databasecluster.wkvl6pz.mongodb.net/find_my_restaurant_db?retryWrites=true&w=majority",
+};
