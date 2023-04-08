3358be933911b50ad3cca556bc1c4c4d91989638
Prathamesh Lakhpati <prathmeshlakhpaty@gmail.com>
2023-04-08 13:27:39 +0530

    Changed Database configuration

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,5 +1,4 @@
 module.exports = {
   DB_NAME: "find-my-restaurant-db",
-  DB_URL:
-    "mongodb+srv://prathmeshlakhpaty:9764prath123@databasecluster.wkvl6pz.mongodb.net/find_my_restaurant_db?retryWrites=true&w=majority",
+  DB_URL: "mongodb://localhost:27017",
 };
